03623cebda2873bf3317c0898fecde035c8d09fd
Dmitry Shevchenko <duosora1997@gmail.com>
2023-08-28 18:45:17 +0300

    Horse class test end

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
 
 class HorseTest {
 
@@ -27,7 +30,7 @@ class HorseTest {
 
     @Test
     public void ConstructorWithThirdParamNegativeDistanceAndExceptionMessage() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Ghost", 20.0,-200.0));
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Ghost", 20.0, -200.0));
         assertEquals("Distance cannot be negative.", exception.getMessage());
     }
 
@@ -40,17 +43,39 @@ class HorseTest {
 
     @Test
     void getSpeed() {
+        Horse horse = new Horse("Ghost", 20.0, 200.0);
+        assertEquals(20.0, horse.getSpeed());
     }
 
     @Test
     void getDistance() {
+        Horse horse = new Horse("Ghost", 20.0, 200.0);
+        assertEquals(200.0, horse.getDistance());
     }
 
     @Test
-    void move() {
+    void getDefaultDistance() {
+        Horse horse = new Horse("Ghost", 20.0);
+        assertEquals(0, horse.getDistance());
     }
 
     @Test
-    void getRandomDouble() {
+    void moveRandom() {
+        try(MockedStatic<Horse> mocked = mockStatic(Horse.class)) {
+            new Horse("Ghost", 20.0,200.0).move();
+            mocked.verify(() -> Horse.getRandomDouble(0.2,0.9));
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.2, 0.4, 0.6, 0.8})
+    public void moveDistance(double random) {
+        try (MockedStatic<Horse> mocked = mockStatic(Horse.class)) {
+            mocked.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(random);
+            Horse horse = new Horse("Test", 1, 5);
+            double distance = horse.getDistance() + horse.getSpeed() * Horse.getRandomDouble(0.2, 0.9);
+            horse.move();
+            assertEquals(distance, horse.getDistance());
+        }
     }
 }
